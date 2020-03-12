cfac52e70965e41c2ace4ad17d8047b239123f1c
Grant <jpg170030@utdallas.edu>
2020-03-12 17:12:02 -0500

    Project transfer
    Replacing the original project with the actual work I did for the ica before we started learning about github

---
+++ InclassDemo8/Assets/Parachute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parachute : MonoBehaviour
+{
+    [SerializeField] private float drag = 0f;
+    [SerializeField] private float parachuteDrag = 10f;
+    [SerializeField] GameObject parachute;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = drag;
+        parachute.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, 5f))
+        {
+            rb.drag = parachuteDrag;
+            parachute.SetActive(true);
+        }
+    }
+}
